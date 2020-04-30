eb070f4afaec36faddd5a649febcc3f6554c1a1f
Anoia <esther.machata@gmail.com>
2020-04-30 17:18:24 +0200

    Add logic to display, create and edit recipes

--- src/Main.elm
+++ src/Main.elm
@@ -1,15 +1,9 @@
 module Main exposing (..)
 
--- Press buttons to increment and decrement a counter.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/buttons.html
---
-
-
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, b, button, div, h1, h3, input, label, li, p, text, ul)
+import Html.Attributes exposing (type_, value)
+import Html.Events exposing (onClick, onInput)
 
 
 
@@ -18,24 +12,44 @@ import Html.Events exposing (onClick)
 
 main : Program Int Model Msg
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Int
+type ViewStatus
+    = ListRecipesView
+    | SingleRecipeView Int Recipe
+    | EditRecipeView ( Maybe Int, Recipe )
 
 
-init :  Int -> ( Model, Cmd Msg )
+type alias Recipe =
+    { name : String, ingredients : String, instructions : String }
+
+
+type alias Model =
+    { count : Int
+    , status : ViewStatus
+    , recipes : List Recipe
+    }
+
+
+init : Int -> ( Model, Cmd Msg )
 init x =
-  (x, Cmd.none)
+    let
+        initialRecipes =
+            [ { name = "recipe1", ingredients = "ingr1, ingr2", instructions = "some instructions go here" }
+            , { name = "recipe2", ingredients = "ingr5, ingr7", instructions = "some other instructions go here" }
+            ]
+    in
+    ( { count = x, recipes = initialRecipes, status = ListRecipesView }, Cmd.none )
 
 
 
@@ -43,18 +57,98 @@ init x =
 
 
 type Msg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+    | ShowEditCreateRecipe (Maybe ( Int, Recipe ))
+    | ShowList
+    | ShowSingle Int Recipe
+    | AddRecipe (Maybe Int) Recipe
+    | EditRecipe (Maybe Int) Recipe RecipeUpdate
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+type RecipeUpdate
+    = ChangedName String
+    | ChangedIngredients String
+    | ChangedInstructions String
+
+
+getRecipeOrEmpty : Maybe ( Int, Recipe ) -> ( Maybe Int, Recipe )
+getRecipeOrEmpty maybeRecipe =
+    case maybeRecipe of
+        Just a ->
+            ( Just (Tuple.first a), Tuple.second a )
+
+        Nothing ->
+            ( Nothing, { name = "", ingredients = "", instructions = "" } )
+
+
+noCmd : Model -> ( Model, Cmd Msg )
+noCmd model =
+    ( model, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      (model + 1, Cmd.none)
+    updateNoCmd msg model |> noCmd
 
-    Decrement ->
-      (model - 1, Cmd.none)
+
+updateNoCmd : Msg -> Model -> Model
+updateNoCmd msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
+
+        Decrement ->
+            { model | count = model.count - 1 }
+
+        ShowEditCreateRecipe maybeRecipe ->
+            { model | status = EditRecipeView (getRecipeOrEmpty maybeRecipe) }
+
+        ShowList ->
+            { model | status = ListRecipesView }
+
+        AddRecipe maybeId r ->
+            let
+                updatedRecipes =
+                    Maybe.map (removeRecipeByIndex model.recipes) maybeId
+                        |> Maybe.withDefault model.recipes
+            in
+            { model | recipes = r :: updatedRecipes, status = ListRecipesView }
+
+        ShowSingle int recipe ->
+            { model | status = SingleRecipeView int recipe }
+
+        EditRecipe maybeId recipe newRecipeUpdated ->
+            updateNewRecipe model maybeId recipe newRecipeUpdated
+
+
+removeRecipeByIndex : List Recipe -> Int -> List Recipe
+removeRecipeByIndex recipes index =
+    List.filter (byIndex index) (List.indexedMap Tuple.pair recipes)
+        |> List.map Tuple.second
+
+
+byIndex : Int -> ( Int, Recipe ) -> Bool
+byIndex index tuple =
+    not (Tuple.first tuple == index)
+
+
+updateNewRecipe : Model -> Maybe Int -> Recipe -> RecipeUpdate -> Model
+updateNewRecipe model maybeId recipe recipeUpdate =
+    { model | status = EditRecipeView ( maybeId, applyChangeToRecipe recipe recipeUpdate ) }
+
+
+applyChangeToRecipe : Recipe -> RecipeUpdate -> Recipe
+applyChangeToRecipe recipe recipeUpdate =
+    case recipeUpdate of
+        ChangedName string ->
+            { recipe | name = string }
+
+        ChangedIngredients string ->
+            { recipe | ingredients = string }
+
+        ChangedInstructions string ->
+            { recipe | instructions = string }
 
 
 
@@ -63,15 +157,78 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ div []
+            [ button [ onClick Decrement ] [ text "minus" ]
+            , div [] [ text (String.fromInt model.count) ]
+            , button [ onClick Increment ] [ text "plus" ]
+            ]
+        , div [] [ currentRecipeView model ]
+        ]
+
+
+currentRecipeView : Model -> Html Msg
+currentRecipeView model =
+    case model.status of
+        ListRecipesView ->
+            div []
+                [ createRecipesListView model.recipes
+                , button [ onClick (ShowEditCreateRecipe Nothing) ] [ text "add new" ]
+                ]
+
+        SingleRecipeView id recipe ->
+            div []
+                [ h3 [] [ text recipe.name ]
+                , b [] [ text "Ingredients" ]
+                , p [] [ text recipe.ingredients ]
+                , b [] [ text "Instructions" ]
+                , p [] [ text recipe.instructions ]
+                , button [ onClick ShowList ] [ text "back" ]
+                , button [ onClick (ShowEditCreateRecipe (Just ( id, recipe ))) ] [ text "edit" ]
+                ]
+
+        EditRecipeView ( maybeId, recipe ) ->
+            div []
+                [ h3 [] [ text "Add a new Recipe" ]
+                , p []
+                    [ label [] [ text "Name:" ]
+                    , viewInput "text" recipe.name (\a -> EditRecipe maybeId recipe (ChangedName a))
+                    ]
+                , p []
+                    [ label [] [ text "Ingredients:" ]
+                    , viewInput "text" recipe.ingredients (\a -> EditRecipe maybeId recipe (ChangedIngredients a))
+                    ]
+                , p []
+                    [ label [] [ text "Instructions:" ]
+                    , viewInput "text" recipe.instructions (\a -> EditRecipe maybeId recipe (ChangedInstructions a))
+                    ]
+                , button [ onClick ShowList ] [ text "discard" ]
+                , button [ onClick (AddRecipe maybeId recipe) ] [ text "save" ]
+                ]
+
+
+createRecipesListView : List Recipe -> Html Msg
+createRecipesListView recipes =
+    div []
+        [ h1 [] [ text "List of Recipes" ]
+        , ul [] (List.map createRecipeView (List.indexedMap Tuple.pair recipes))
+        ]
+
+
+createRecipeView : ( Int, Recipe ) -> Html Msg
+createRecipeView ( id, recipe ) =
+    li [ onClick (ShowSingle id recipe) ] [ text recipe.name ]
+
+
+viewInput : String -> String -> (String -> msg) -> Html msg
+viewInput t v toMsg =
+    input [ type_ t, value v, onInput toMsg ] []
+
 
 
 -- SUBSCRIPTIONS
 
+
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.none
+    Sub.none
